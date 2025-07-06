2a4c5f840cc2436f8b325ec9b8563aa9b1d811c3
dwisetbu@gmail.com <dwisetbu@gmail.com>
2025-07-06 20:18:01 +0700

    fix: update data type in price

--- models/product.go
+++ models/product.go
@@ -4,6 +4,6 @@ type Products struct {
 	Id			uint		`json:"id" gorm:"primaryKey"`
 	Title		string		`json:"title"`
 	Description	string		`json:"description"`
-	Price		string		`json:"price"`
+	Price		float64		`json:"price"`
 	Image		string		`json:"image"`
 }
