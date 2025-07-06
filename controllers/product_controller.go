6829ba78cc75510f685f97ac63880d0f22063551
dwisetbu@gmail.com <dwisetbu@gmail.com>
2025-07-06 20:20:04 +0700

    feat: add endpoint create products

---
+++ controllers/product_controller.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"backend/database"
+	"backend/models"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func CreateProduct(c *gin.Context){
+	c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, 10<<20)
+	title := c.PostForm("title")
+	description := c.PostForm("description")
+	priceStr := c.PostForm("price")
+
+	file, err := c.FormFile("image")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Image file requaired"})
+		return
+	}
+
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	if ext != ".jpg" && ext != ".jpeg" && ext != ".png" && ext != ".webp" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Only JPG, JPEG, PNG, WEBP allowed"})
+		return
+	}
+
+	filename := fmt.Sprintf("%d%s", time.Now().UnixNano(), ext)
+	path := filepath.Join("uploads", filename)
+	if err := c.SaveUploadedFile(file, path); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failde to save image"})
+		return
+	}
+
+	price, _ := strconv.ParseFloat(priceStr, 64)
+	product := models.Products{Title: title, Description: description, Price: price, Image: filename}
+	if err := database.DB.Create(&product).Error; err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create product"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"success": true, "data": product})
+}
